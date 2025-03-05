0634d012b04e212026039b0dd8dbc313c44e84c6
sailakshmy <sailakshmy94@gmail.com>
2025-03-05 06:33:17 +0400

    Added styles via CSS Modules

--- EventsProject/components/events/event-item.js
+++ EventsProject/components/events/event-item.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import classes from "./event-item.module.css";
 
 const EventItem = (props) => {
   const { title, location, date, id, image } = props.event;
@@ -11,19 +12,22 @@ const EventItem = (props) => {
   const formattedAddress = location.replace(", ", "\n");
   const exploreEventLink = `/events/${id}`;
   return (
-    <li>
-      {/* <img src={`/${image}`} alt={title} /> */}
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time datetime="">{readableDate}</time>
+    <li className={classes.item}>
+      <img src={`/${image}`} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time datetime="">{readableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address> {formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <address> {formattedAddress}</address>
+
+        <div className={classes.actions}>
+          <Link href={exploreEventLink}>Explore Event</Link>
         </div>
-      </div>
-      <div>
-        <Link href={exploreEventLink}>Explore Event</Link>
       </div>
     </li>
   );
--- EventsProject/components/events/event-list.js
+++ EventsProject/components/events/event-list.js
@@ -1,9 +1,10 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 function EventList(props) {
   const { events } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {events?.map((event) => (
         <EventItem event={event} key={event?.id} />
       ))}
