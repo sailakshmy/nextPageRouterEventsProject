d4b073a02a412d044318d57ceb74844f007054dc
sailakshmy <sailakshmy94@gmail.com>
2025-03-24 06:35:23 +0400

    Added Server side rendering to render the catch al routes page

--- EventsProject/pages/events/[...events].js
+++ EventsProject/pages/events/[...events].js
@@ -1,17 +1,55 @@
-import { useRouter } from "next/router";
 import React from "react";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
-import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-utils";
 
-const OtherEvents = () => {
-  const router = useRouter();
-  const filterData = router.query.events;
-  if (!filterData) {
-    return <p className="center">Loading...</p>;
+const OtherEvents = ({ hasError, events, eventDate }) => {
+  // const router = useRouter();
+  // const filterData = router.query.events;
+  // if (!filterData) {
+  //   return <p className="center">Loading...</p>;
+  // }
+  // const numYear = +filterData?.[0]; // Transforming string to Number using +;
+  // const numMonth = +filterData?.[1]; // Transforming string to Number using +;
+
+  if (hasError) {
+    return (
+      <div className="center">
+        <ErrorAlert>Invalid filters, please adjust your values</ErrorAlert>
+        <Button link="/events">Back To Events page</Button>
+      </div>
+    );
   }
+  // const events = getFilteredEvents({
+  //   year: numYear,
+  //   month: numMonth,
+  // });
+
+  if (!events || events?.length === 0) {
+    return (
+      <div className="center">
+        <ErrorAlert>No event found for the filter</ErrorAlert>
+        <Button link="/events">Back To Events page</Button>
+      </div>
+    );
+  }
+
+  const date = new Date(eventDate.numYear, eventDate.numMonth - 1);
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList events={events} />
+    </>
+  );
+};
+
+export default OtherEvents;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const filterData = params.events;
   const numYear = +filterData?.[0]; // Transforming string to Number using +;
   const numMonth = +filterData?.[1]; // Transforming string to Number using +;
 
@@ -23,32 +61,24 @@ const OtherEvents = () => {
     numMonth < 1 ||
     numMonth > 13
   ) {
-    return (
-      <div className="center">
-        <ErrorAlert>Invalid filters, please adjust your values</ErrorAlert>
-        <Button link="/events">Back To Events page</Button>
-      </div>
-    );
+    return {
+      props: {
+        hasError: true,
+      },
+    };
   }
-  const events = getFilteredEvents({
+  const events = await getFilteredEvents({
     year: numYear,
     month: numMonth,
   });
-  if (!events || events?.length === 0) {
-    return (
-      <div className="center">
-        <ErrorAlert>No event found for the filter</ErrorAlert>
-        <Button link="/events">Back To Events page</Button>
-      </div>
-    );
-  }
-  const date = new Date(numYear, numMonth - 1);
-  return (
-    <>
-      <ResultsTitle date={date} />
-      <EventList events={events} />
-    </>
-  );
-};
 
-export default OtherEvents;
+  return {
+    props: {
+      events: events,
+      eventDate: {
+        numYear,
+        numMonth,
+      },
+    },
+  };
+}
