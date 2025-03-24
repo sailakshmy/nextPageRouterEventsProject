43fe6f83a01f54bcc7e4b1a90adb113f47a719ae
sailakshmy <sailakshmy94@gmail.com>
2025-03-24 07:02:17 +0400

    Changed the server side rendering logic to client side fetching with useSWR

--- EventsProject/pages/events/[...events].js
+++ EventsProject/pages/events/[...events].js
@@ -1,20 +1,52 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
-import { getFilteredEvents } from "../../helpers/api-utils";
 
-const OtherEvents = ({ hasError, events, eventDate }) => {
-  // const router = useRouter();
-  // const filterData = router.query.events;
-  // if (!filterData) {
-  //   return <p className="center">Loading...</p>;
-  // }
-  // const numYear = +filterData?.[0]; // Transforming string to Number using +;
-  // const numMonth = +filterData?.[1]; // Transforming string to Number using +;
+const OtherEvents = () => {
+  const router = useRouter();
+  const filterData = router.query.events;
+  const [loadedEvents, setLoadedEvents] = useState();
 
-  if (hasError) {
+  const { data, error } = useSWR(
+    "https://nextjs-dummyproject-37a39-default-rtdb.firebaseio.com/events.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    console.log("Data", data);
+    if (data) {
+      const events = [];
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const numYear = +filterData?.[0]; // Transforming string to Number using +;
+  const numMonth = +filterData?.[1]; // Transforming string to Number using +;
+
+  // if (hasError) {
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 13 ||
+    error
+  ) {
     return (
       <div className="center">
         <ErrorAlert>Invalid filters, please adjust your values</ErrorAlert>
@@ -27,7 +59,15 @@ const OtherEvents = ({ hasError, events, eventDate }) => {
   //   month: numMonth,
   // });
 
-  if (!events || events?.length === 0) {
+  const filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
+
+  if (!filteredEvents || filteredEvents?.length === 0) {
     return (
       <div className="center">
         <ErrorAlert>No event found for the filter</ErrorAlert>
@@ -36,49 +76,49 @@ const OtherEvents = ({ hasError, events, eventDate }) => {
     );
   }
 
-  const date = new Date(eventDate.numYear, eventDate.numMonth - 1);
+  const date = new Date(numYear, numMonth - 1);
   return (
     <>
       <ResultsTitle date={date} />
-      <EventList events={events} />
+      <EventList events={loadedEvents} />
     </>
   );
 };
 
 export default OtherEvents;
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const filterData = params.events;
-  const numYear = +filterData?.[0]; // Transforming string to Number using +;
-  const numMonth = +filterData?.[1]; // Transforming string to Number using +;
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+//   const filterData = params.events;
+//   const numYear = +filterData?.[0]; // Transforming string to Number using +;
+//   const numMonth = +filterData?.[1]; // Transforming string to Number using +;
 
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 13
-  ) {
-    return {
-      props: {
-        hasError: true,
-      },
-    };
-  }
-  const events = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 13
+//   ) {
+//     return {
+//       props: {
+//         hasError: true,
+//       },
+//     };
+//   }
+//   const events = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
 
-  return {
-    props: {
-      events: events,
-      eventDate: {
-        numYear,
-        numMonth,
-      },
-    },
-  };
-}
+//   return {
+//     props: {
+//       events: events,
+//       eventDate: {
+//         numYear,
+//         numMonth,
+//       },
+//     },
+//   };
+// }
