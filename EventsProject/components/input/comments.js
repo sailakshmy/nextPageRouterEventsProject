4851b993bfa6bc5a8d01cba02f1f7db288a21fc6
sailakshmy <sailakshmy94@gmail.com>
2025-04-08 07:18:12 +0400

    Added  new input components

---
+++ EventsProject/components/input/comments.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import CommentList from "./comment-list";
+import classes from "./comments.module.css";
+import NewComment from "./new-comment";
+
+function Comments(props) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+}
+
+export default Comments;
