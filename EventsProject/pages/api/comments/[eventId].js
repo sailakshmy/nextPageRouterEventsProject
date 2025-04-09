89abf5ad4645a8162572d52a0ab2fa36575cdfb8
sailakshmy <sailakshmy94@gmail.com>
2025-04-09 06:57:04 +0400

    Added an API route for comments

---
+++ EventsProject/pages/api/comments/[eventId].js
@@ -0,0 +1,52 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+  if (req.method === "POST") {
+    const { email, name, comment } = req.body;
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({
+        message: "Invalid inputs",
+      });
+      return;
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text: comment,
+    };
+    console.log("data", newComment);
+    res.status(201).json({
+      message: "Added comments",
+      comment: newComment,
+    });
+  }
+  if (req.method === "GET") {
+    const dummyComments = [
+      {
+        id: "c1",
+        name: "xyz",
+        email: "s@s.com",
+        text: "Some comments",
+      },
+      {
+        id: "c2",
+        name: "xyaz",
+        email: "s@as.com",
+        text: "Some comments again",
+      },
+    ];
+    res.send(200).json({
+      comments: dummyComments,
+    });
+  }
+}
+
+export default handler;
