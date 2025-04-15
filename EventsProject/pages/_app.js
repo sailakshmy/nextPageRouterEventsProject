2c0aba0508d4264a7ac6411084f9af390dbdbf5d
sailakshmy <sailakshmy94@gmail.com>
2025-04-15 06:41:49 +0400

    Created the Notification Context

--- EventsProject/pages/_app.js
+++ EventsProject/pages/_app.js
@@ -4,14 +4,19 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Events</title>
-        <meta name="description" content="Events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Events</title>
+          <meta name="description" content="Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
---
+++ EventsProject/store/notification-context.js
@@ -0,0 +1,34 @@
+import { createContext } from "react";
+
+export const NotificationContext = createContext({
+  notification: null,
+  showNotification: ({ title, message, status }) => {}, // Function to show a notification
+  hideNotification: () => {}, // Function to hide a notification
+});
+
+const NotificationContextProvider = (props) => {
+  const [activeNotification, setActiveNotification] = useState();
+  const showNotificationHandler = (notificationData) => {
+    // Logic to show a notification
+    setActiveNotification(notificationData);
+  };
+
+  const hideNotificationHandler = () => {
+    // Logic to hide a notification
+    setActiveNotification(null);
+  };
+
+  const value = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={value}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+};
+
+export default NotificationContextProvider;
