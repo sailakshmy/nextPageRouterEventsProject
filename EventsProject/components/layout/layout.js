0ebc3887556dc7caaeecbee736dcc269c53a2404
sailakshmy <sailakshmy94@gmail.com>
2025-04-15 06:48:24 +0400

    Used the useContext hook to display the notification

--- EventsProject/components/layout/layout.js
+++ EventsProject/components/layout/layout.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { NotificationContext } from "../../store/notification-context";
+import Notification from "../ui/notification";
 import MainHeader from "./main-header";
 
 const Layout = (props) => {
+  const notificationContext = useContext(NotificationContext);
+  const activeNotification = notificationContext.notification;
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      <Notification
+        title={activeNotification?.title}
+        message={activeNotification?.message}
+        status={activeNotification?.status}
+      />
     </>
   );
 };
